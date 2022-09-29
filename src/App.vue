d7ea6b32d556f27ee3e2e52080fce1450244e005
oliverounaid <oliverounaid@gmail.com>
2022-09-29 12:08:02 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Tutorial from './components/Tutorial.vue'
-import Score from './components/Score.vue'
-import Game from './components/Game.vue'
+import MainContent from './components/MainContent.vue'
+
 </script>
 
 <template>
-  <Tutorial/>
-  <Game/>
-  <Score />
+  <MainContent/>
 </template>
 
 <style scoped>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+function changeContent() {
+  document.getElementById("playButton")
+}
+</script>
+
+<template>
+  <!-- <section id="tutorial">
+    <h1>Tutorial</h1>
+    <span>
+    Õpi morse koodi nagu töeline meremees. Mänguekraanile kuvatakse ülesanne, millele pead vastama kasutades morse koodi.
+    Morse koodi sisestamiseks kasuta 'Space' klahvi või hiirt. (Täpsustus)
+    </span>
+    <button id="playButton" type="button">Mängi!</button>
+  </section> -->
+  <section id="gameMain" class="flex flex-col">
+    <div>
+      <h2 id="question">A</h2>
+    </div>
+    <input class="inputBox" type="text" value="">
+    <div>
+       <button class="flex" onclick="morseClick()">Space</button>
+    </div>
+    <div>
+      
+    </div>
+   
+  </section>
+  <section id="gameOver">
+
+  </section>
+</template>
+
+<style scoped>
+
+</style>
