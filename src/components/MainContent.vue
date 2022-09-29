a07e368ec73d79c7957f573ce484ff6b1014d489
oliverounaid <oliverounaid@gmail.com>
2022-09-29 16:41:25 +0300

    k

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,15 +1,44 @@
 <script setup>
 import { ref } from 'vue'
+import alphabet from '../assets/alphabet.json'
 
 defineProps({
   msg: String
 })
 
+// Socket.io server configuration
+const isOnline = true;
+
+const serverURL = "https://kak-socketio-server.herokuapp.com";
+// serverURL = "http://localhost:8000";
+
+if ( isOnline ) {
+  const socket = io(serverURL);
+  
+  socket.on("connect", () => {
+    console.log("Client connected to: " + socket.id);
+    // online = true;
+  });
+  
+  socket.on("send-data", res => {
+    console.log(res);
+    if ("t" in res) { 
+      signalCheck(res.t);
+    }
+  });
+  
+  socket.on("disconnect", () => {
+    console.log("Client disconnected from" + socket.id); 
+  });
+}
+
 const count = ref(0)
 
+const letters = alphabet.chars;
 const mouseDownVal = ref(0);
 const mouseUpVal = ref(0);
 const myLetter = ref("");
+let timeoutID ;
 
 
 function mouseDown() {
@@ -23,15 +52,36 @@ function mouseUp() {
 function checkLength() {
   const pressTime = mouseUpVal.value - mouseDownVal.value;
 
+  signalCheck(pressTime);
+}
+function signalCheck(pressTime) {
   if (pressTime < 140 ) {
     myLetter.value += ".";
-    console.log(".")
+    // console.log(".")
   } else {
     myLetter.value += "-";
-    console.log("-")
+    // console.log("-")
   }
+  console.log(myLetter.value,letters[0].A);
+  if (letters[0].A.startsWith(myLetter.value)) {
+    console.log("ok")
+  } else {
+    console.log("not ok")
+  }
+  clearTimeout(timeoutID);
+  endCheck();
 }
-
+function endCheck() {
+  timeoutID = setTimeout(() => {
+    console.error("time up");
+    // myLetter.value = "";
+    if (myLetter.value === letters[0].A) {
+      console.log("great success")
+    } else {
+      console.log("try again")
+    }
+  }, 2000);
+}
 </script>
 
 <template>
@@ -47,7 +97,7 @@ function checkLength() {
     <div>
       <h2 id="question">A</h2>
     </div>
-    <p id="inputBox">{{myLetter}}</p>
+    <p id="inputBox"> {{myLetter}}</p>
     <div>
       <button id="clicker" @mousedown="mouseDown" @mouseup="mouseUp" class="flex">Space</button>
     </div>
